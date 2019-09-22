4cc40de3c3d3924da72f4670831277ec27deda02
Alexandr <5056671@gmail>
2019-09-22 16:00:48 +0300

    22.09

--- AndroidAcademyOne/app/src/main/java/com/example/androidacademyone/MainActivity.kt
+++ AndroidAcademyOne/app/src/main/java/com/example/androidacademyone/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.example.androidacademyone
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button: Button = findViewById(R.id.button)
+        button.setOnClickListener {
+            openSecondActivity()
+        }
+    }
+
+    private fun openSecondActivity() {
+        val intent = Intent(this, SecondActivity::class.java)
+        startActivity(intent)
     }
 }
